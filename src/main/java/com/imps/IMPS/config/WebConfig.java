41f8a3e76009fd9f2f6f0f18115a6ed7f3b459a1
bydear18 <yacademiko@gmail.com>
2024-10-20 16:44:39 +0800

    Update

--- src/main/java/com/imps/IMPS/config/WebConfig.java
+++ src/main/java/com/imps/IMPS/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.imps.IMPS.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("https://impsonline-production.up.railway.app") // Use your frontend URL
+                .allowedOrigins("http://localhost:3000") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*") 
                 .allowCredentials(true); 
