c67c07151b47bdf7083cf0b8bb908442b08a5740
bydear18 <yacademiko>
2024-10-21 16:57:32 +0800

    changes

--- src/main/java/com/imps/IMPS/config/WebConfig.java
+++ src/main/java/com/imps/IMPS/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("https://imps-online-front-9nhnz4ikl-hyuxjins-projects.vercel.app") 
+                .allowedOrigins("https://front-end-imps.vercel.app") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*") 
                 .allowCredentials(true); 
